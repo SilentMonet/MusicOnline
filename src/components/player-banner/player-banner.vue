2b3ff932c7c7f7488009f99bcced249613a74543
SilentMonet <1204988681@qq.com>
2018-09-11 20:35:40 +0800

    修复页面初始化时的播放BUG

--- src/components/player-banner/player-banner.vue
+++ src/components/player-banner/player-banner.vue
@@ -9,6 +9,10 @@
         v-bind:class="currentModeClass"
         v-on:click="currentModeIndex=[currentModeIndex+1]%playModes.length"
       ></i>
+      <i
+        class="icon-next"
+        v-on:click="playNext"
+      ></i>
       <i 
         v-bind:class="[isPlaying?'icon-pause':'icon-play']" 
         v-on:click="isPlaying === true ? pausePlay() : startPlay()"
@@ -21,6 +25,8 @@
         v-on:playing="isPlaying=true" 
         v-on:pause="isPlaying=false" 
         v-on:timeupdate="currentTime=$refs.audio.currentTime"
+        v-on:ended="playNext"
+        v-on:canplay="startPlay"
         preload="auto"
         ></audio>
 
@@ -53,20 +59,24 @@ export default {
   },
   watch: {
     currentPlay: function(newPlay) {
-      this.startPlay();
+      this.$refs.audio.currentTime = 0;
+      this.isPlaying=false;
+      this.$refs.audio.load();
     }
   },
   methods: {
     startPlay() {
-      this.$refs.audio.currentTime = 0;
       let playPromise = this.$refs.audio.play();
+      
       if (playPromise !== undefined) {
         playPromise
-          .then(_ => {
+          .then(() => {
             console.log("start to play");
+            
           })
           .catch(error => {
-            console.log("play failed");
+            console.log(error);
+            this.isPlaying=false;
           });
       }
     },
@@ -74,13 +84,32 @@ export default {
       this.$refs.audio.pause();
     },
     playNext() {
-      store.state.currentIndex =
-        (store.state.currentIndex + 1) % store.state.currentList.length;
+      switch(this.playModes[this.currentModeIndex]){
+        case 'loop-list':
+          store.state.currentIndex = (store.state.currentIndex + 1) % store.state.currentList.length;
+          break;
+        case 'loop-one':
+          this.$refs.audio.currentTime=0;
+          this.startPlay();
+          break;
+        case 'shuffle':
+          let index=Math.round((store.state.currentList.length-1)*Math.random());
+          if( store.state.currentIndex===index ){
+            this.$refs.audio.currentTime=0;
+          this.startPlay();
+          }
+          else{
+            store.state.currentIndex=index;
+          }
+          break;      
+      }
+      
     }
   },
   mounted: function() {
   },
-  beforeDestroy: function() {}
+  beforeDestroy: function() {
+  }
 };
 </script>
 <style>
@@ -99,10 +128,10 @@ export default {
 .artist {
   margin: 0;
   padding: 0;
+  
 }
 .song {
   display: inline-block;
-  width: 100%;
   max-width: 250px;
   min-height: 50px;
   padding-left: 46px;
@@ -120,7 +149,9 @@ export default {
 }
 .artist {
   color: #808080;
-}
+  font-size: 12px;
+  line-height: 1.5;
+  }
 .icon-music {
   position: absolute;
   top: 14px;
@@ -132,24 +163,37 @@ export default {
 .icon-pause,
 .icon-loop-list,
 .icon-loop-one,
-.icon-shuffle {
+.icon-shuffle,
+.icon-next{
   float: right;
   font-size: 24px;
   margin: 12px 4px 12px;
   color: rgba(0, 0, 0, 0.5);
 }
+
 .icon-loop-list,
 .icon-loop-one,
 .icon-shuffle{
+  width: 26px;
   margin-right: 8px;
+}
+.icon-shuffle{
+  margin-top: 10px;
 }
 .icon-loop-one{
   font-size: 18px;
   font-weight: bold;
-  margin-top: 15px;
+  margin-top: 14px;
 }
 .icon-loop-list{
-  margin-top: 14px;
+  margin-top: 13px;
+  font-size: 18px;
+  font-weight: bolder;
+}
+.icon-next{
+  
   font-size: 22px;
+  margin-left: 0;
+  font-weight: bolder;
 }
 </style>
