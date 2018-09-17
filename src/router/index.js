db944985e54945af631807cbe01c6aee388d66e6
SilentMonet <1204988681@qq.com>
2018-09-17 17:35:24 +0800

    启用vue-router进行页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
-/* jshint esversion: 6 */ 
+/* jshint esversion: 6 */
 import Vue from 'vue';
 import Router from 'vue-router';
-import list from '../components/main-container/list/list';
-import search from '../components/main-container/search/search';
+import lists from '../components/content-view/lists/lists';
+import searcher from '../components/content-view/searcher/searcher';
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/list',
-      name: 'list',
-      component: list
+      path: '/lists',
+      name: 'lists',
+      component: lists
     },
     {
-      path:'/search',
-      name:'search',
-      component:search
+      path:'/searcher',
+      name:'searcher',
+      component:searcher
     }
   ]
 });
